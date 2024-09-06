e177eb5ad6e501b1cce0d6353ce4573cbc02299f
kimchanjun <rlackswns7@gmail.com>
2024-09-06 22:49:14 +0900

    #18 목업 페이지 위치변경

--- src/pages/admin-management-pages/NoticeListPage/NoticeListPage.jsx
+++ src/pages/admin-management-pages/NoticeListPage/NoticeListPage.jsx
@@ -4,7 +4,7 @@ export default function NoticeListPage({navigation}) {
     return(<View style={styles.container}>
         <Text style={styles.pageText}>공지사항 리스트 페이지</Text>
     <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('NoticePost')}>
-        <Text style={styles.buttonText}>공지사항  등록</Text>
+        <Text style={styles.buttonText}>공지사항  등록(관리자)</Text>
     </TouchableOpacity>
         <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('NoticeView')}>
             <Text style={styles.buttonText}>공지사항  이동</Text>
