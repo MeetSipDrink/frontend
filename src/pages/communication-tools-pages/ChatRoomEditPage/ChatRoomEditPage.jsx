30452524b481d17fa448c0d9a28206045f42ab00
kimchanjun <rlackswns7@gmail.com>
2024-09-06 22:54:43 +0900

    #20  채팅 생성 페이지 목업 구현

--- src/pages/communication-tools-pages/ChatRoomEditPage/ChatRoomEditPage.jsx
+++ src/pages/communication-tools-pages/ChatRoomEditPage/ChatRoomEditPage.jsx
@@ -1,21 +1,24 @@
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {StyleSheet, View, Text, TouchableOpacity} from "react-native";
+import React from "react";
 
-export default function ChatRoomListPage({ navigation }) {
-    return(<View style={styles.container}>
-            <Text style={styles.pageText}>채팅방 목록 페이지</Text>
-            <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('ChatRoomEdit')}>
-                <Text  style={styles.buttonText}>채팅방 만들기</Text>
-            </TouchableOpacity>
+export default function ChatRoomEditPage({ navigation }) {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.pageText}>채팅방 만들기 페이지</Text>
+            <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('ChatRoomList')}>
+                <Text style={styles.buttonText}>채팅방 리스트 (뒤로가기)</Text>
+                </TouchableOpacity>
             <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('ChatRoom')}>
-                <Text style={styles.buttonText}>채팅방 이동</Text>
+                <Text style={styles.buttonText}>채팅방  만들기</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.Button} onPress={() => navigation.navigate('Home')}>
+            <TouchableOpacity style={styles.Button} onPress={() =>navigation.navigate('Home') }>
                 <Text style={styles.buttonText}>홈으로</Text>
             </TouchableOpacity>
         </View>
 
     )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
