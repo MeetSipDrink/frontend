6ab4a91ab978af70d9456b20df971d21fb63164b
kimchanjun <rlackswns7@gmail.com>
2024-09-17 23:08:59 +0900

    보드 리스트 개선

--- src/pages/board-system-pages/useRefreshPosts/useRefreshPosts.jsx
+++ src/pages/board-system-pages/useRefreshPosts/useRefreshPosts.jsx
@@ -3,27 +3,28 @@ import axios from 'axios';
 
 const API_URL = 'http://10.0.2.2:8080';
 
-const useRefreshPosts = (size, sortBy, searchKeyword, searchOption, setPosts, setPage, setHasMore) => {
+const useRefreshPosts = (size, sortBy, setPosts, setPage, setHasMore, setSearchKeyword, setSearchOption) => {
     const [refreshing, setRefreshing] = useState(false);
 
     const refreshPosts = useCallback(async () => {
         if (refreshing) return;
         setRefreshing(true);
         try {
-            const url = `${API_URL}/posts/search/?page=0&size=${size}&keyword=${searchKeyword}&sort=${sortBy}&searchOption=${searchOption}`;
+            const url = `${API_URL}/posts/search/?page=0&size=${size}&keyword=&sort=${sortBy}&searchOption=title`;
             console.log(`Refreshing: ${url}`);
             const response = await axios.get(url);
             const newPosts = response.data.data;
             setPosts(newPosts);
-            setPage(1); // 새로고침 후 페이지를 1로 설정
+            setPage(1);
             setHasMore(newPosts.length === size);
-
+            setSearchKeyword(''); // 검색어 초기화
+            setSearchOption('title'); // 검색 옵션 초기화
         } catch (error) {
             console.error('새로고침 중 오류 발생:', error);
         } finally {
             setRefreshing(false);
         }
-    }, [size, sortBy, searchKeyword, searchOption, setPosts, setPage, setHasMore, refreshing]);
+    }, [size, sortBy, setPosts, setPage, setHasMore, setSearchKeyword, setSearchOption]);
 
     return { refreshing, refreshPosts };
 };
