6c3455a33bdf5bf83fe26afa0b8cc8ba922f5567
kimchanjun <rlackswns7@gmail.com>
2024-09-17 22:48:15 +0900

    컴포넌트 분리

--- src/pages/board-system-pages/useRefreshPosts/useRefreshPosts.jsx
+++ src/pages/board-system-pages/useRefreshPosts/useRefreshPosts.jsx
@@ -1,20 +1,21 @@
 import { useState, useCallback } from 'react';
+import axios from 'axios';
 
 const API_URL = 'http://10.0.2.2:8080';
 
-const useRefreshPosts = (size, sortBy, setPosts, setPage, setHasMore) => {
+const useRefreshPosts = (size, sortBy, searchKeyword, searchOption, setPosts, setPage, setHasMore) => {
     const [refreshing, setRefreshing] = useState(false);
 
     const refreshPosts = useCallback(async () => {
         if (refreshing) return;
         setRefreshing(true);
         try {
-            const response = await fetch(`${API_URL}/posts/search/?page=0&size=${size}&keyword=&sort=${sortBy}`);
-            const result = await response.json();
-
-            const newPosts = result.data;
+            const url = `${API_URL}/posts/search/?page=0&size=${size}&keyword=${searchKeyword}&sort=${sortBy}&searchOption=${searchOption}`;
+            console.log(`Refreshing: ${url}`);
+            const response = await axios.get(url);
+            const newPosts = response.data.data;
             setPosts(newPosts);
-            setPage(1); // 새로고침 후 페이지를 초기화
+            setPage(1); // 새로고침 후 페이지를 1로 설정
             setHasMore(newPosts.length === size);
 
         } catch (error) {
@@ -22,7 +23,7 @@ const useRefreshPosts = (size, sortBy, setPosts, setPage, setHasMore) => {
         } finally {
             setRefreshing(false);
         }
-    }, [sortBy, size, setPosts, setPage, setHasMore, refreshing]);
+    }, [size, sortBy, searchKeyword, searchOption, setPosts, setPage, setHasMore, refreshing]);
 
     return { refreshing, refreshPosts };
 };
